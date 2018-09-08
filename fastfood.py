585b3703060d8f1637e28ebba25ef048cda69bf4
Philip Otieno <tmitchellb007@gmail.com>
2018-09-08 21:02:14 +0300

    Added user registartion details

--- fastfood.py
+++ fastfood.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,7 +11,16 @@ app.config['SECRET_KEY'] = "philipotieno"
 def home():
 	return jsonify({'message' : 'welcome to Fast-Food-Fast'}), 200
 
+#Register new user
+@app.route('/api/v1/register', methods=['POST'])
+def register():
+	name = request.get_json()['name']
+	username = request.get_json()['username']
+	email = request.get_json()['email']
+	password = request.get_json()['password']
+
+	return jsonify({"message" : "registration succesfull"})
 
 #Initalization
 if __name__=="__main__":
-	app.run(debug = True)
+	app.run(debug = True, port=5001)
