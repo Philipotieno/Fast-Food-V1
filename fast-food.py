441bba8c97f02c633c5ef6e010522543e801e658
Philip Otieno <tmitchellb007@gmail.com>
2018-09-08 12:29:59 +0300

    Added a homepage route

---
+++ fast-food.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = "philipotieno"
+
+
+#Homepage route
+@app.route('/api/v1/', methods=['GET'])
+def home():
+	return jsonify({'message' : 'welcome to Fast-Food-Fast'}), 200
+
+
+
+#Initalization
+if __name__=="__main__":
+	app.run(debug = True)
