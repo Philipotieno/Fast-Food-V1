e2ab2d018997cf2273deb2ab135be816141aff63
Philip Otieno <tmitchellb007@gmail.com>
2018-09-09 10:54:05 +0300

    Added a log in route

--- fastfood.py
+++ fastfood.py
@@ -23,6 +23,13 @@ def register():
 		user.update({username:{"name":name, "email":email, "password":password}})
 	return jsonify(user), 200
 
+#Login if registered
+@app.route('/api/v1/login', methods=['POST'])
+def login():
+	username = request.get_json()['username']
+	password = request.get_json()['password']
+
+	return jsonify({'messge' : 'welcome to Fast-Food-Fast'})
 #Initalization
 if __name__=="__main__":
-	app.run(debug = True, port=5001)
+	app.run(debug = True)
